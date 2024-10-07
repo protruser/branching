25f2044954a5e2caec0c1f2f68304af11bea0e86
protruser <marokk9768@gmail.com>
2024-10-07 23:23:19 +0900

    creating substract function

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+  public static void main(String[] args) {
+    //using int function
+    System.out.println("5 + 5 = " + sum(5,5));
+  }
+
+  private static int sum(int a, int b) {
+    return a + b;
+  }
+
+  private static int sum(int a, int b) {
+    // todo: finish this function
+    return 0;
+  }
+}
